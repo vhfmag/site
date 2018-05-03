5a136a43b3ddf098077a6a3400298c7855f789a2
Victor Magalhães <victorhfmag@gmail.com>
2018-05-02 22:44:42 -0300

    refactor: Move types to global scope & declare modules

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,6 @@
 declare const graphql: (query: TemplateStringsArray) => void;
+declare type DeepNonNullable<T> = { [K in keyof T]-?: NonNullable<T[K]> };
+declare type NonNullableNode = NonNullable<React.ReactNode>;
+
+declare module "typography";
+declare module "typography-theme-grand-view";
