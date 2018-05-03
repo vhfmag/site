d2abeca66fcab164cd35d4ddb123b54ed9c24fb7
Victor Magalhães <victorhfmag@gmail.com>
2018-05-03 10:57:02 -0300

    fix: Fix typo

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,11 @@
 import Typography from "typography";
 import grandViewTheme from "typography-theme-grand-view";
-import { GraphQLString } from "graphql";
 
 const typography = new Typography({
 	...grandViewTheme,
 	googleFonts: [
 		// @ts-ignore
-		...grandViewTheme.filter(({ name }) => name !== "Arvo"),
+		...grandViewTheme.googleFonts.filter(({ name }) => name !== "Arvo"),
 		{
 			name: "Zilla Slab",
 			styles: ["400", "400i", "700", "700i"],
