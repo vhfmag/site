61b7142bad70752a86844386ba8e80a03dbd159a
Victor Magalhães <victorhfmag@gmail.com>
2018-05-07 21:14:06 -0300

    fix: Fix manifests' display property typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
 				start_url: "/",
 				background_color: backgroundColor,
 				theme_color: themeColor,
-				display: "minimal_ui",
+				display: "minimal-ui",
 				icon: "src/assets/images/icons/icon.png",
 			},
 		},
