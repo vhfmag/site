897fa8862bbb74da8f72a58b9ba1c1dc94387a22
Victor Magalhães <victorhfmag@gmail.com>
2018-05-08 00:30:31 -0300

    feat: Enable rss feed!

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@ const {
 	textColor,
 } = require("./src/utils/consts");
 
+const {
+	extractFileNameFromPath,
+} = require("./src/utils/utils");
+
 module.exports = {
 	siteMetadata: {
 		title: "Victor Magalhães",
@@ -69,5 +73,69 @@ module.exports = {
 			},
 		},
 		"gatsby-plugin-sitemap",
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
+				{
+				  site {
+					siteMetadata {
+					  title
+					  description
+					  siteUrl
+					  site_url: siteUrl
+					}
+				  }
+				}
+			  `,
+				feeds: [{
+					serialize: ({
+						query: {
+							site,
+							allMarkdownRemark
+						}
+					}) => {
+						return allMarkdownRemark.edges.map(edge => {
+							const slug = extractFileNameFromPath(edge.node.fileAbsolutePath);
+							const url = `${site.siteMetadata.siteUrl}/post/${slug}`;
+
+							return Object.assign({}, edge.node.frontmatter, {
+								description: edge.node.excerpt,
+								url,
+								guid: url,
+								custom_elements: [{
+									"content:encoded": edge.node.html
+								}],
+							});
+						});
+					},
+					query: `
+					{
+					  allMarkdownRemark(
+						limit: 1000,
+						sort: { order: DESC, fields: [frontmatter___date] },
+						filter: {
+							frontmatter: { draft: { ne: true } },
+							fileAbsolutePath: {regex: "/posts/"}
+						}
+					  ) {
+						edges {
+						  node {
+							excerpt
+							html
+							fileAbsolutePath
+							frontmatter {
+							  title
+							  date
+							}
+						  }
+						}
+					  }
+					}
+				  `,
+					output: "/rss.xml",
+				}, ],
+			},
+		},
 	],
 };
