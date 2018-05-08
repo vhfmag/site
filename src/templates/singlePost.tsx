089baba7509419ee120d12b65ec26e0619c8c769
Victor Magalhães <victorhfmag@gmail.com>
2018-05-08 01:25:02 -0300

    feat: Include structured data on blog posts

--- src/templates/singlePost.tsx
+++ src/templates/singlePost.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import { formatDate } from "../utils/utils";
 import { Article } from "../components/Article";
+import { Helmet } from "react-helmet";
 
 interface IPostTemplateProps {
 	data: {
+		site: {
+			siteMetadata: {
+				siteUrl: string;
+			};
+		};
+		personalJson: {
+			name: string;
+		};
 		markdownRemark: {
 			htmlAst: string;
+			excerpt: string;
 			frontmatter: {
 				title: string;
 				description: string;
@@ -25,8 +35,13 @@ export default class SinglePostTemplate extends React.Component<
 	public render() {
 		const {
 			data: {
+				site: {
+					siteMetadata: { siteUrl },
+				},
+				personalJson: { name },
 				markdownRemark: {
 					htmlAst,
+					excerpt,
 					frontmatter: { date, title, description },
 				},
 			},
@@ -34,22 +49,56 @@ export default class SinglePostTemplate extends React.Component<
 
 		const formattedDate = formatDate(date);
 
+		const linkedData = `{
+			"@context": "http://schema.org",
+			"@type": "BlogPosting",
+			"mainEntityOfPage": {
+				"@type": "Blog",
+				"@id": "${siteUrl}"
+			},
+			"headline": "${title}",
+			"datePublished": "${date}",
+			"author": {
+				"@type": "Person",
+				"name": "${name}"
+			},
+			"publisher": {
+				"@type": "Person",
+				"name": "${name}"
+			},
+			"description": "${excerpt}"
+		}`;
+
 		return (
-			<Article
-				title={title}
-				subtitle={description}
-				info={formattedDate}
-				htmlAst={htmlAst}
-				url="#"
-			/>
+			<>
+				<Helmet>
+					<script type="application/ld+json">{linkedData}</script>
+				</Helmet>
+				<Article
+					title={title}
+					subtitle={description}
+					info={formattedDate}
+					htmlAst={htmlAst}
+					url="#"
+				/>
+			</>
 		);
 	}
 }
 
 export const pageQuery = graphql`
 	query SinglePostQuery($markdownPath: String!) {
+		site {
+			siteMetadata {
+				siteUrl
+			}
+		}
+		personalJson {
+			name
+		}
 		markdownRemark(fileAbsolutePath: { eq: $markdownPath }) {
 			htmlAst
+			excerpt
 			frontmatter {
 				title
 				description
