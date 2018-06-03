a8a84095e06f8ae2e7e918b45fa7921a24d12368
Victor Magalhães <victorhfmag@gmail.com>
2018-06-03 01:22:57 -0300

    feat: Add support for multiple font sizes of Highlight component

--- src/components/articles/Highlight/index.tsx
+++ src/components/articles/Highlight/index.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { Float } from "../Float";
 
-export const Highlight = styled(Float)`
-	font-size: 2em;
+export const Highlight = styled<{ size?: "small" | "medium" | "large" }>(Float)`
+	font-size: ${({ size }) =>
+		size === "small" ? "1.25em" : size === "medium" ? "1.5em" : "2em"};
 	line-height: 1;
 `;
