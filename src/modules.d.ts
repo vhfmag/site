87bbc01a87b8e921ac465af8f8d1b206f5eaae90
Victor Magalhães <victorhfmag@gmail.com>
2018-12-08 10:07:38 -0300

    Install @reach's SkipNav and VisuallyHidden

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -2,3 +2,17 @@ declare module "typography";
 declare module "rehype-react";
 declare module "gray-percentage";
 declare module "typography-breakpoint-constants";
+
+declare module "@reach/visually-hidden" {
+	import * as React from "react";
+	const VisuallyHidden: React.ComponentType;
+	export default VisuallyHidden;
+}
+
+declare module "@reach/skip-nav" {
+	import * as React from "react";
+	export const SkipNavContent: React.ComponentType;
+	export const SkipNavLink: React.ComponentType<
+		React.HTMLProps<HTMLAnchorElement>
+	>;
+}
