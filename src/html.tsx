0cbfb774461783fc209f83045821a6b1883eae1a
Victor Magalhães <victorhfmag@gmail.com>
2019-04-13 11:01:52 -0300

    Minify theme FOUS preventing script

--- src/html.tsx
+++ src/html.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import terser from "terser";
 import PropTypes from "prop-types";
 
 export default function HTML(props: any) {
@@ -21,45 +22,45 @@ export default function HTML(props: any) {
 			<script
 				type="text/javascript"
 				dangerouslySetInnerHTML={{
-					__html: `
-        (function() {
-          // Change these if you use something different in your hook.
-          var storageKey = 'currentTheme';
-          var classNameDark = 'dark';
-          var classNameLight = 'light';
-        
-          function setClassOnDocumentBody(darkMode) {
-            document.body.classList.add(darkMode ? classNameDark : classNameLight);
-            document.body.classList.remove(darkMode ? classNameLight : classNameDark);
-          }
+					__html: terser.minify(`
+          (function() {
+            // Change these if you use something different in your hook.
+            var storageKey = 'currentTheme';
+            var classNameDark = 'dark';
+            var classNameLight = 'light';
           
-          var preferDarkQuery = '(prefers-color-scheme: dark)';
-          var mql = window.matchMedia(preferDarkQuery);
-          var supportsColorSchemeQuery = mql.media === preferDarkQuery;
-          var localStorageTheme = null;
-          try {
-            localStorageTheme = localStorage.getItem(storageKey);
-          } catch (err) {}
-          var localStorageExists = localStorageTheme !== null;
-          if (localStorageExists) {
-            localStorageTheme = JSON.parse(localStorageTheme);
-          }
-        
-          // Determine the source of truth
-          if (localStorageExists) {
-            // source of truth from localStorage
-            setClassOnDocumentBody(localStorageTheme);
-          } else if (supportsColorSchemeQuery) {
-            // source of truth from system
-            setClassOnDocumentBody(mql.matches);
-            localStorage.setItem(storageKey, mql.matches);
-          } else {
-            // source of truth from document.body
-            var isDarkMode = document.body.classList.contains(classNameDark);
-            localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
-          }
-        })();
-      `,
+            function setClassOnDocumentBody(darkMode) {
+              document.body.classList.add(darkMode ? classNameDark : classNameLight);
+              document.body.classList.remove(darkMode ? classNameLight : classNameDark);
+            }
+            
+            var preferDarkQuery = '(prefers-color-scheme: dark)';
+            var mql = window.matchMedia(preferDarkQuery);
+            var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+            var localStorageTheme = null;
+            try {
+              localStorageTheme = localStorage.getItem(storageKey);
+            } catch (err) {}
+            var localStorageExists = localStorageTheme !== null;
+            if (localStorageExists) {
+              localStorageTheme = JSON.parse(localStorageTheme);
+            }
+          
+            // Determine the source of truth
+            if (localStorageExists) {
+              // source of truth from localStorage
+              setClassOnDocumentBody(localStorageTheme);
+            } else if (supportsColorSchemeQuery) {
+              // source of truth from system
+              setClassOnDocumentBody(mql.matches);
+              localStorage.setItem(storageKey, mql.matches);
+            } else {
+              // source of truth from document.body
+              var isDarkMode = document.body.classList.contains(classNameDark);
+              localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
+            }
+          })();
+          `).code!,
 				}}
 			/>
 		</html>
