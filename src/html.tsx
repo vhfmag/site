5925985b974fbedec6c6c7bae842e906e04cf931
Victor Magalhães <victorhfmag@gmail.com>
2019-04-16 16:29:02 -0300

    Remove unused meta tag

--- src/html.tsx
+++ src/html.tsx
@@ -7,7 +7,6 @@ export default function HTML(props: any) {
 		<html {...props.htmlAttributes}>
 			<head>
 				<meta charSet="utf-8" />
-				<meta httpEquiv="x-ua-compatible" content="ie=edge" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, shrink-to-fit=no"
