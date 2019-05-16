b7e124c251e84d16e7ea56ba2bdad93a053e79e7
Victor Magalhães <victorhfmag@gmail.com>
2019-05-16 08:32:12 -0300

    Revert "Stop relying on tree shaking for icons"
    This reverts commit 9474866ba5a574a2b4b13e2b584a5d3c8ebbb5e2.

--- src/components/layout/icons.tsx
+++ src/components/layout/icons.tsx
@@ -1,21 +1,23 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-
-import { faRss } from "@fortawesome/free-solid-svg-icons/faRss";
-import { faPenNib } from "@fortawesome/free-solid-svg-icons/faPenNib";
-import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
-import { faBookmark } from "@fortawesome/free-solid-svg-icons/faBookmark";
-import { faCircle } from "@fortawesome/free-solid-svg-icons/faCircle";
-import { faChalkboard } from "@fortawesome/free-solid-svg-icons/faChalkboard";
-
-import { faMastodon } from "@fortawesome/free-brands-svg-icons/faMastodon";
-import { faGitlab } from "@fortawesome/free-brands-svg-icons/faGitlab";
-import { faGetPocket } from "@fortawesome/free-brands-svg-icons/faGetPocket";
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
-import { faKeybase } from "@fortawesome/free-brands-svg-icons/faKeybase";
-import { faMedium } from "@fortawesome/free-brands-svg-icons/faMedium";
+import {
+	faRss,
+	faPenNib,
+	faBook,
+	faBookmark,
+	faCircle,
+	faChalkboard,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+	faMastodon,
+	faGitlab,
+	faGetPocket,
+	faGithub,
+	faLinkedin,
+	faTwitter,
+	faInstagram,
+	faKeybase,
+	faMedium,
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(faRss);
 library.add(faPenNib);
