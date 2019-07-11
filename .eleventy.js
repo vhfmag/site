525d7a1cf0180807578b3758fb22fef0400f0ae3
Victor Magalhães <victorhfmag@gmail.com>
2019-07-11 06:38:47 -0300

    Initial RSS setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,23 @@
 const excerptPlugin = require("eleventy-plugin-excerpt");
 const pwaPlugin = require("eleventy-plugin-pwa");
 const cacheBusterPlugin = require("@mightyplow/eleventy-plugin-cache-buster");
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 const fs = require("fs");
 
 const { figureShortcode } = require("./src/_shortcodes/figure");
+
+function addCollection(eleventyConfig, collectionName, collectionFolder) {
+	eleventyConfig.addCollection(collectionName, collection => {
+		let files = [...collection.getFilteredByGlob(`./src/${collectionFolder}/**/*.md`)];
+
+		if (process.env.NODE_ENV !== "development") {
+			files = files.filter(p => !p.data.draft);
+		}
+
+		return files.reverse();
+	});
+}
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css");
@@ -14,21 +27,15 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(excerptPlugin);
 	eleventyConfig.addPlugin(cacheBusterPlugin({ outputDirectory: "public" }));
 	eleventyConfig.addPlugin(pwaPlugin);
+	eleventyConfig.addPlugin(rssPlugin);
 
 	const collectionNames = fs
 		.readdirSync("./src/")
 		.filter(path => !path.startsWith("_") && fs.statSync(`./src/${path}`).isDirectory());
 
+	addCollection(eleventyConfig, "tudo", `(${collectionNames.join("|")})`);
 	for (const collectionName of collectionNames) {
-		eleventyConfig.addCollection(collectionName, collection => {
-			let files = [...collection.getFilteredByGlob(`./src/${collectionName}/**/*.md`)];
-
-			if (process.env.NODE_ENV !== "development") {
-				files = files.filter(p => !p.data.draft);
-			}
-
-			return files.reverse();
-		});
+		addCollection(eleventyConfig, collectionName, collectionName);
 	}
 
 	eleventyConfig.addFilter("formatDateISO", date => {
