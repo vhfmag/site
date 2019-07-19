4a41e30bde211376fb55f988f35a75fbc8f35951
Victor Magalhães <victorhfmag@gmail.com>
2019-07-19 20:55:07 -0300

    Try to setup indiekit

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css");
 	eleventyConfig.addPassthroughCopy("js");
 	eleventyConfig.addPassthroughCopy("icons");
+	eleventyConfig.addPassthroughCopy("uploads");
 	eleventyConfig.addPassthroughCopy(".well-known");
 
 	eleventyConfig.setLibrary(
