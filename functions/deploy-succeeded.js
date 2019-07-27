d7740ec57ed640b453268bcafe84e44bc41da96c
Victor Magalhães <victorhfmag@gmail.com>
2019-07-27 08:34:24 -0300

    Iterate on webmention function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -4,5 +4,8 @@ const { exec } = require("child-process");
 const promiseExec = promisify(exec);
 
 module.exports = async function sendWebmentions(event, context, callback) {
-	await promiseExec("npx webmention https://victormagalhaes.codes/feed.all.xml --send --debug");
+	const results = await promiseExec(
+		"npx webmention https://victormagalhaes.codes/feed.all.xml --send --debug",
+	);
+	callback(null, { statucCode: 200, body: JSON.stringify(results) });
 };
