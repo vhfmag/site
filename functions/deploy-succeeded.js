39a21efc9c469e98921705e3c508fe9f47b9e03a
Victor Magalhães <victorhfmag@gmail.com>
2019-10-23 16:13:48 -0300

    Fix deploy succeeded hook

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,12 +1,15 @@
 const { promisify } = require("util");
+const { join } = require("path");
 const { exec } = require("child_process");
 
 const promiseExec = promisify(exec);
 
+const binPath = join(__dirname, "../node_modules/@remy/webmention/bin/wm.js");
+
 exports.handler = async function sendWebmentions(event, context, callback) {
 	console.log("sendWebmentions was successfully called!");
 	const results = await promiseExec(
-		"npx webmention https://victormagalhaes.codes/feed.all.xml --send --debug",
+		`${binPath} https://victormagalhaes.codes/feed.all.xml --send --debug`,
 	);
 	callback(null, { statucCode: 200, body: JSON.stringify(results) });
 };
