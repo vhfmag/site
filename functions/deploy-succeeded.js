22098b1dadc49e04bc0cc258204be04930e06227
Victor Magalhães <victorhfmag@gmail.com>
2019-10-23 14:44:17 -0300

    Fix typo

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,9 +1,10 @@
 const { promisify } = require("util");
-const { exec } = require("child-process");
+const { exec } = require("child_process");
 
 const promiseExec = promisify(exec);
 
 module.exports = async function sendWebmentions(event, context, callback) {
+	console.log("sendWebmentions was successfully called!");
 	const results = await promiseExec(
 		"npx webmention https://victormagalhaes.codes/feed.all.xml --send --debug",
 	);
