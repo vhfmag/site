d2e117adf1b71f80c32dc85e64cc94512800667b
Victor Magalhães <victorhfmag@gmail.com>
2019-11-12 07:11:55 -0300

    Fix image optimization plugin

--- plugins/images.js
+++ plugins/images.js
@@ -1,25 +1,27 @@
 const { JSDOM } = require("jsdom");
 const { promisify } = require("util");
+const { join } = require("path");
 const { memoize } = require("lodash");
 const sizeOf = memoize(promisify(require("image-size")));
 
 /* TODO: use https://npm.im/webp-converter to add a webp source */
 /* TODO: use https://npm.im/cacache or what else to cache this shit */
 
-const transformImgPath = (/** @type {string} */ src) => {
+const transformImgPath = (/** @type {string} */ src, outputPath) => {
 	if (src.startsWith("/") && !src.startsWith("//")) {
 		return `.${src}`;
+	} else {
+		const outputDir = join(outputPath, "..").replace("/public/", "/src/");
+		return `${outputDir}/${src}`;
 	}
-
-	return src;
 };
 
 const cachePath = "../node_modules/.cache/optimize-images-11ty-plugin";
 
-const processImage = async (/** @type {HTMLImageElement} */ img) => {
+const processImage = outputPath => async (/** @type {HTMLImageElement} */ img) => {
 	if (img.src.match(/^https?:\/.*/)) return;
 
-	const srcPath = transformImgPath(img.src.split("?")[0]);
+	const srcPath = transformImgPath(img.src.split("?")[0], outputPath);
 	const { width, height } = await sizeOf(srcPath);
 	img.setAttribute("width", width);
 	img.setAttribute("height", height);
@@ -42,7 +44,7 @@ async function transformMarkup(rawContent, outputPath) {
 
 		if (images.length > 0) {
 			console.log(`found ${images.length} images in ${outputPath}`);
-			await Promise.all(images.map(processImage));
+			await Promise.all(images.map(processImage(outputPath)));
 			console.log(`processed ${images.length} images in ${outputPath}`);
 
 			content = dom.serialize();
