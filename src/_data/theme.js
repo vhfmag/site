cb80f31e3d59f186d70a0c39d93c552e55cd70c0
Victor Magalhães <victorhfmag@gmail.com>
2020-05-16 09:58:57 -0300

    Design tokens FTW

---
+++ src/_data/theme.js
@@ -0,0 +1,14 @@
+module.exports = {
+	light: {
+		textColor: "hsl(0, 0%, 0%)",
+		backgroundColor: "rgb(231, 179, 195)",
+		themeColor: "hsl(207.1, 93.1%, 20.4%)",
+		mutedThemeColor: "hsla(207.1, 93.1%, 20.4%, 0.4)",
+	},
+	dark: {
+		textColor: "hsl(0, 0%, 93%)",
+		backgroundColor: "rgb(5, 79, 140)",
+		themeColor: "rgb(231, 179, 195)",
+		mutedThemeColor: "rgba(231, 179, 195, 0.4)",
+	},
+};
