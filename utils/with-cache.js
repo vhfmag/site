c1b190680749624d1362e1faa05efa9437ea1f23
Victor Magalhães <victorhfmag@gmail.com>
2020-06-14 21:20:49 +0000

    Implement fetchWithCache with withCache

--- utils/with-cache.js
+++ utils/with-cache.js
@@ -6,45 +6,39 @@ const CACHE_PATH = require("path").join(__dirname, "../node_modules/.cache/cacac
 const ONE_HOUR = 3_600_000;
 const SIX_HOURS = 6 * ONE_HOUR;
 
-async function fetchWithCache(input, init, ttl = SIX_HOURS) {
-	const cacheKey = typeof input === "string" ? input : input.url;
-
-	const isCached = await cacache.get.info(CACHE_PATH, cacheKey);
-	if (isCached && Date.now() - isCached.metadata.timestamp < ttl) {
-		const cacheEntry = await cacache.get(CACHE_PATH, cacheKey);
-		return JSON.parse(cacheEntry.data);
-	}
-
-	const result = await fetch(input, init).then(res => {
-		if (res.ok) {
-			return res.json();
-		} else {
-			throw new Error(`Status code ${res.status}: ${res.statusText}`);
-		}
-	});
-
-	await cacache.put(CACHE_PATH, cacheKey, JSON.stringify(result), {
-		metadata: { timestamp: Date.now() },
-	});
-
-	return result;
-}
+const fetchWithCache = withCache(
+	input => (typeof input === "string" ? input : input.url),
+	(...args) =>
+		fetch(...args).then(res => {
+			if (res.ok) {
+				return res.json();
+			} else {
+				throw new Error(
+					`Status code ${res.status}: ${res.statusText} for ${JSON.stringify(
+						{ input, init },
+						undefined,
+						4,
+					)}`,
+				);
+			}
+		}),
+);
 
 function withCache(key, asyncFn, ttl = SIX_HOURS) {
 	const fnName = `${asyncFn.name || "anonymousFunction"}WithCache`;
 	return {
 		[fnName]: async (...args) => {
-			const hasContent = await cacache.get.info(CACHE_PATH, key);
-			if (hasContent) {
-				const cacheEntry = await cacache.get(CACHE_PATH, key);
-				if (Date.now() - cacheEntry.metadata.timestamp < ttl) {
-					return JSON.parse(cacheEntry.data);
-				}
+			const cacheKey = typeof key === "function" ? key(...args) : key;
+
+			const isCached = await cacache.get.info(CACHE_PATH, cacheKey);
+			if (isCached && Date.now() - isCached.metadata.timestamp < ttl) {
+				const cacheEntry = await cacache.get(CACHE_PATH, cacheKey);
+				return JSON.parse(cacheEntry.data);
 			}
 
 			const res = await asyncFn(...args);
 
-			await cacache.put(CACHE_PATH, key, JSON.stringify(res), {
+			await cacache.put(CACHE_PATH, cacheKey, JSON.stringify(res), {
 				metadata: { timestamp: Date.now() },
 			});
 
