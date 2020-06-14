2e9f1cf49a7c3c855f4930c7e8e657112a650862
Victor Magalhães <victorhfmag@gmail.com>
2020-06-14 15:55:28 +0000

    Remove logs from cache utility

--- utils/with-cache.js
+++ utils/with-cache.js
@@ -3,19 +3,16 @@ const cacache = require("cacache");
 const CACHE_PATH = require("path").join(__dirname, "../node_modules/.cache/cacache");
 
 const ONE_HOUR = 3_600_000;
+const FOUR_HOURS = 4 * ONE_HOUR;
 
-function withCache(key, asyncFn, ttl = ONE_HOUR) {
+function withCache(key, asyncFn, ttl = FOUR_HOURS) {
 	const fnName = `${asyncFn.name || "anonymousFunction"}WithCache`;
 	return {
 		[fnName]: async () => {
-			console.log(`${fnName} has been called`);
 			const hasContent = await cacache.get.info(CACHE_PATH, key);
-			console.log({ hasContent });
 			if (hasContent) {
 				const cacheEntry = await cacache.get(CACHE_PATH, key);
-				console.log({ cacheEntry });
 				if (Date.now() - cacheEntry.metadata.timestamp < ttl) {
-					console.log(`${key} has been fetched from cache`);
 					return JSON.parse(cacheEntry.data);
 				}
 			}
@@ -25,7 +22,6 @@ function withCache(key, asyncFn, ttl = ONE_HOUR) {
 			await cacache.put(CACHE_PATH, key, JSON.stringify(res), {
 				metadata: { timestamp: Date.now() },
 			});
-			console.log(`Cache for ${key} has been updated`);
 
 			return res;
 		},
