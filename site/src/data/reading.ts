0ee615b6a51f23646a2e22a7e5526516c90694df
Victor Magalhães <victorhfmag@gmail.com>
2023-01-13 12:17:16 -0300

    feat: Implement reading section

---
+++ site/src/data/reading.ts
@@ -0,0 +1,4 @@
+export const booksImSupposedlyReading = [
+	"The Age of Surveillance Capitalism: The Fight for a Human Future at the New Frontier of Power – Shoshana Zuboff",
+	"Ficciones – Jorge Luis Borges",
+];
