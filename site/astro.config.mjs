55fcde3fd5de433cafe170953051367c3f9df766
Victor Magalhães <victorhfmag@gmail.com>
2023-01-29 12:46:14 -0300

    feat: Implement sitemap support

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import remarkGFMPlugin from "remark-gfm";
 import { canonicalUrl } from "./src/data/site";
 import { addAutomaticLayoutPlugin } from "./src/plugins/remark/collectionData";
@@ -7,7 +8,7 @@ import { attributesExtensionPlugin } from "./src/plugins/remark/attributesExtens
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
+	integrations: [mdx(), sitemap()],
 	site: canonicalUrl,
 	markdown: {
 		remarkPlugins: [remarkGFMPlugin, addAutomaticLayoutPlugin, attributesExtensionPlugin],
