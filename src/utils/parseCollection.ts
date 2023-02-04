ecdd4869a081d9775af2e66bf8caba0fe0782fec
Victor Magalhães <victorhugo.freitasmagalhaes@coinbase.com>
2023-02-04 07:46:25 -0300

    fix: Show draft posts while developing

--- src/utils/parseCollection.ts
+++ src/utils/parseCollection.ts
@@ -7,7 +7,7 @@ export function parseCollection(
 	{ requireTitle = true }: ParseCollectionOptions = {},
 ) {
 	return collection
-		.filter(p => (!requireTitle || p.frontmatter.title) && !p.frontmatter.draft)
+		.filter(p => !requireTitle || p.frontmatter.title)
 		.sort(
 			(p1, p2) =>
 				new Date(p2.frontmatter.date).valueOf() - new Date(p1.frontmatter.date).valueOf(),
