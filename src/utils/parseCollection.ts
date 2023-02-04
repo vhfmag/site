2f36f69414be3420b0041a59a0792a021f10b865
Victor Magalhães <victorhfmag@gmail.com>
2023-02-04 14:07:22 -0300

    fix: Filter out drafts in prod

--- src/utils/parseCollection.ts
+++ src/utils/parseCollection.ts
@@ -10,5 +10,6 @@ export function parseCollection(
 ) {
 	return collection
 		.filter(p => !requireTitle || p.data.title)
+		.filter(p => !import.meta.env.PROD || p.data.draft !== true)
 		.sort((p1, p2) => new Date(p2.data.date).valueOf() - new Date(p1.data.date).valueOf());
 }
